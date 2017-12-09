67591d163282093bbbba1c345910177c79177e20
Muhammad Arinal <rinal@email.com>
2017-12-09 09:59:42 +0700

    materi

--- src/pages/materi/materi.ts
+++ src/pages/materi/materi.ts
@@ -14,49 +14,49 @@ import { Observable } from 'rxjs/Observable';
   selector: 'page-materi',
   templateUrl: 'materi.html',
 })
-export class MateriPage {
-	@ViewChild(Slides) slides: Slides;
-  halaman : Observable<any[]>;
-  bab = "";
+ export class MateriPage {
+    @ViewChild(Slides) slides: Slides;
+    isiHalaman : Observable<any[]>;
+    bab = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public firebaseProvider: FirebaseProvider) {
-    let id = navParams.get("key");
-    this.bab = navParams.get("bab");
-    this.halaman = this.firebaseProvider.getBab(id);
-    console.log(id);
+    constructor(public navCtrl: NavController, public navParams: NavParams, public firebaseProvider: FirebaseProvider) {
+        let id = navParams.get("key");
+        this.bab = navParams.get("bab");
+        this.isiHalaman = this.firebaseProvider.getBab(id);
+        console.log(id);
+    }
+
+    halamanChange(){
+      let currentHalaman = this.slides.getActiveIndex()+1;
+      let el = document.getElementById("halaman");
+
+      el.innerHTML = String(currentHalaman);
   }
 
-	halamanChange(){
-		let currentHalaman = this.slides.getActiveIndex()+1;
-	  	let el = document.getElementById("halaman");
+  hideButton() {
+      let backBtn = document.getElementById("back-btn");
+      let nextBtn = document.getElementById("next-btn");
+      let panjangSlides = this.slides.length();
 
-		el.innerHTML = String(currentHalaman);
-	}
+      if (this.slides.getActiveIndex() == 0){
+         backBtn.style.display = "none";
+     } else{
+         backBtn.style.display = "block";
+     }
 
-	hideButton() {
-		let backBtn = document.getElementById("back-btn");
-  		let nextBtn = document.getElementById("next-btn");
-  		let panjangSlides = this.slides.length();
+     if (this.slides.getActiveIndex() == panjangSlides-1) {
+         nextBtn.style.display = "none";
+     }else{
+         nextBtn.style.display = "block";
+     }
 
-  		if (this.slides.getActiveIndex() == 0){
-  			backBtn.style.display = "none";
-  		} else{
-  			backBtn.style.display = "block";
-  		}
+ }
+ nextSlide():void{
+    this.slides.slideNext();
+}
 
-  		if (this.slides.getActiveIndex() == panjangSlides-1) {
-  			nextBtn.style.display = "none";
-  		}else{
-  			nextBtn.style.display = "block";
-  		}
-
-	}
-  	nextSlide():void{
-  		this.slides.slideNext();
-  	}
-
-  	prevSlide():void{
-  		this.slides.slidePrev();
-  	}
+prevSlide():void{
+    this.slides.slidePrev();
+}
 
 }
