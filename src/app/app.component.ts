13ade4b2c9bbb3c51ab4c42cf284d355cbcbe81d
Hammam Afiq Murtadho <hammam.afiq277@gmail.com>
2017-12-13 11:17:03 +0700

    hai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { App, Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -11,13 +11,58 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, app: App, alertCtrl:AlertController, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleBlackTranslucent();
+      statusBar.styleDefault();
       statusBar.backgroundColorByHexString('#f77074');
       splashScreen.hide();
+
+      //let nav = app.getActiveNavs()[0];
+      //let activeView = nav.getActive();
+      //let nameActiveView = activeView.name;
+      //console.log(nav);
     });
+
+    platform.registerBackButtonAction(() => {
+      let nav = app.getActiveNavs()[0];
+      let activeView = nav.getActive();
+
+      if (activeView.name === "QuizPage") {
+
+        //do nothing here to disable hardware back button 
+        
+      } else {
+
+        if (nav.canGoBack()) {
+          
+          nav.pop();
+
+        } else {
+
+          const alert = alertCtrl.create({
+            title: 'App termination',
+            message: 'Do you want to close the app?',
+            buttons: [{
+              text: 'Cancel',
+              role: 'cancel',
+              handler: () => {
+                console.log('Application exit prevented!');
+              }
+            },
+            {
+              text: 'Close App',
+              handler: () => {
+                platform.exitApp();
+              }
+            }]
+          });
+
+          alert.present();
+        }
+      }
+
+    }, 100);
   }
 }
